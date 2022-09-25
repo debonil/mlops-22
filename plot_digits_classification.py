a519d3a01fbff3d24d3c869b33af5f5ce240c306
debonil <debonil.ghosh@gmail.com>
2022-09-25 10:12:31 +0530

    added util file

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -12,72 +12,8 @@ hand-written digits, from 0-9.
 # License: BSD 3 clause
 
 # Standard scientific Python imports
-import matplotlib.pyplot as plt
 from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
-
-
-def data_viz(data_to_viz):
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, label in zip(axes, data_to_viz.images, data_to_viz.target):
-        ax.set_axis_off()
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title("Training: %i" % label)
-
-def data_preprocess(data):
-    # flatten the images
-    n_samples = len(data.images)
-    x = data.images.reshape((n_samples, -1))
-    return x,digits.target
-
-def train_dev_test_split(data, label, train_frac, dev_frac):
-
-    dev_test_frac = 1 - train_frac
-    x_train, x_dev_test, y_train, y_dev_test = train_test_split(
-        data, label, test_size=dev_test_frac, shuffle=True
-    )
-    x_test, x_dev, y_test, y_dev = train_test_split(
-        x_dev_test, y_dev_test, test_size=(dev_frac) / dev_test_frac, shuffle=True
-    )
-
-    return x_train, y_train, x_dev, y_dev, x_test, y_test
-
-
-
-def h_param_tuning(h_param_comb, clf, x_train, y_train, x_dev, y_dev, metric):
-    best_metric = -1.0
-    best_model = None
-    best_h_params = None
-    # 2. For every combination-of-hyper-parameter values
-    for cur_h_params in h_param_comb:
-
-        # PART: setting up hyperparameter
-        hyper_params = cur_h_params
-        clf.set_params(**hyper_params)
-
-        # PART: Train model
-        # 2.a train the model
-        # Learn the digits on the train subset
-        clf.fit(x_train, y_train)
-
-        # print(cur_h_params)
-        # PART: get dev set predictions
-        predicted_dev = clf.predict(x_dev)
-
-        # 2.b compute the accuracy on the validation set
-        cur_metric = metric(y_pred=predicted_dev, y_true=y_dev)
-
-        # 3. identify the combination-of-hyper-parameter for which validation set accuracy is the highest.
-        if cur_metric > best_metric:
-            best_metric = cur_metric
-            best_model = clf
-            best_h_params = cur_h_params
-            print("Found new best metric with :" + str(cur_h_params))
-            print("New best val metric:" + str(cur_metric))
-    return best_model, best_metric, best_h_params
-
-
-
+from util import data_preprocess,data_viz,h_param_tuning,train_dev_test_split,visualize_pred_data
 ## Starts actual execution
 
 digits = datasets.load_digits()
@@ -114,3 +50,20 @@ metric=metrics.accuracy_score
 best_model, best_metric, best_h_params = h_param_tuning(h_param_comb, clf, X_train, y_train, X_dev, y_dev, metric)
 
 print(f"\nBest Classification {metric} for classifier {best_model} is {best_metric:.2f}\n")
+
+
+# PART: Get test set predictions
+# Predict the value of the digit on the test subset
+predicted = best_model.predict(X_test)
+
+visualize_pred_data(X_test,predicted)
+
+# 4. report the test set accurancy with that best model.
+# PART: Compute evaluation metrics
+print(
+    f"Classification report for classifier {clf}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+)
+
+print("Best hyperparameters were:")
+print(best_h_params)
