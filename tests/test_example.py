436dd4e272e2ab0ca9f2dec7a5ebc01b0cdaed53
debonil <debonil.ghosh@gmail.com>
2022-10-23 11:30:40 +0530

    Quiz 3 responses

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,4 @@
 
 
-def test_example():
-    assert 1==1
-
-
-def test_non_test_file():
-    print("test_non_tesrt_file**")
-    assert 1==1
 
     
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,14 +1,46 @@
 import sys
+from sklearn import datasets, metrics, svm
+import numpy
 sys.path.append('.')
 
-from util import generate_h_param_comb
+from util import generate_h_param_comb,data_preprocess,data_viz,train_dev_test_split,visualize_pred_data
 
 # test case to check if all the combinations of the hyper parameters are indeed getting created
-def test_generate_h_param_comb():
+def removed_test_generate_h_param_comb():
     gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
     c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
     h_param_comb = generate_h_param_comb(gamma_list,c_list)
     assert len(h_param_comb)==len(gamma_list)*len(c_list)
+
+# test case to check if all the combinations of the hyper parameters are indeed getting created
+def test_quize_quest3():
+    data, label = data_preprocess(datasets.load_digits())
+    metric=metrics.accuracy_score
+    X_train, y_train, X_dev, y_dev, X_test, y_test = train_dev_test_split(
+        data, label, 0.8, 0.1
+    )
+    best_metric = {'gamma': 0.001, 'C': 2}
+    best_model = svm.SVC()
+    best_model.set_params(**best_metric)
+    best_model.fit(X_train, y_train)
+    predicted = best_model.predict(X_test)
+    unique_pred_classes = numpy.unique(predicted)
+    assert len(unique_pred_classes)>1
+
+def test_quize_quest4():
+    data, label = data_preprocess(datasets.load_digits())
+    X_train, y_train, X_dev, y_dev, X_test, y_test = train_dev_test_split(
+        data, label, 0.8, 0.1
+    )
+    best_metric = {'gamma': 0.001, 'C': 2}
+    best_model = svm.SVC()
+    best_model.set_params(**best_metric)
+    best_model.fit(X_train, y_train)
+    predicted = best_model.predict(X_test)
+    unique_pred_classes = numpy.unique(predicted)
+    #print(unique_pred_classes)
+    #print(numpy.unique(label))
+    assert len(unique_pred_classes)>=len(numpy.unique(label))
 
 
 #what more test cases should be there 
